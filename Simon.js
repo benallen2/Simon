9871cb311d193c0f3377e96d8696d6f5de9ff567
Ben Allen <ben.allen2@gmail.com>
2016-08-20 13:31:50 -0400

    changes

--- Simon.js
+++ Simon.js
@@ -1,5 +1,5 @@
 var simon = [];
-var counter = 0;
+var counter = 1;
 var choices = "";
 
 
@@ -8,41 +8,27 @@ function pattern(){
 	var rand = Math.floor(Math.random() * 4);
 	console.log(rand);
 	if (rand === 0){
-		simon.push("red")
-		$("#red").addClass("redflash");//do this more generically later.
-		setTimeout(function(){
-			$("#red").removeClass("redflash");
-		}, 800);
-		console.log(simon);
+		simon.push("red")		
 	}
 	else if (rand === 1){
 		simon.push("green")
-		$("#green").addClass("greenflash");//see above
-		setTimeout(function(){
-			$("#green").removeClass("greenflash");
-		}, 800);
-		console.log(simon);
 	}
 	else if (rand === 2){
 		simon.push("blue")
-		$("#blue").addClass("blueflash");
-		setTimeout(function(){
-			$("#blue").removeClass("blueflash");
-		}, 800);
-		console.log(simon);
 	}
 	else if (rand === 3){
 		simon.push("yellow")
-		$("#yellow").addClass("yellowflash");
-		setTimeout(function(){
-			$("#yellow").removeClass("yellowflash");
-		}, 800);
-		console.log(simon);
 	}
-	})
+});
+	for (var i = 0; i < simon.length; i++){
+		simon.forEach(flash(simon[i]));
+		}
+	
 }
 
-
+function flash(elem){
+	$("#" + elem).addClass(elem + "flash");
+}
 
 
 
@@ -61,4 +47,6 @@ function startHover(){
 $(document).ready(function(){
 	startHover();
 	pattern();
+	$(".patterncount").html("# of flashes: " + counter);
+
 })
